c7cde0a1bf989e21704b7b2db2a442eab068e3de
Rahul Dhiman <rahul.dhiman.mohanlal@gmail.com>
2026-01-27 00:30:31 +0530

    update

--- student/certificate/download-certificate.php
+++ student/certificate/download-certificate.php
@@ -143,6 +143,7 @@ try {
             top: 280px; /* Moved down slightly */
             left: 50px;
             width: 100px; height: 120px; border: 1px solid #000;
+            margin-top: -40px;
         }
         
         .content-text {
