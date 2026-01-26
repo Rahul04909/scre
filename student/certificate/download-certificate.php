cda79bafa49a3534d323de0da88becf562e2c860
Rahul Dhiman <rahul.dhiman.mohanlal@gmail.com>
2026-01-27 00:31:31 +0530

    updated marksheet

--- student/certificate/download-certificate.php
+++ student/certificate/download-certificate.php
@@ -143,7 +143,7 @@ try {
             top: 280px; /* Moved down slightly */
             left: 50px;
             width: 100px; height: 120px; border: 1px solid #000;
-            margin-top: -40px;
+            margin-top: -120px;
         }
         
         .content-text {
