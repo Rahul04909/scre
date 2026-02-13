c57fda8a692508716eed78accdded597bf2cbf8a
Rahul Dhiman <rahul.dhiman.mohanlal@gmail.com>
2026-02-13 19:01:36 +0530

    updated

--- admin/certificate/generate-certificate.php
+++ admin/certificate/generate-certificate.php
@@ -3,6 +3,11 @@ session_start();
 require_once '../../database/config.php';
 require_once '../../vendor/autoload.php';
 
+// Enable Errors
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 use Dompdf\Dompdf;
 use Dompdf\Options;
 
